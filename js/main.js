5ab6db1a4f86fda398130fa5e7d35b71e0861ae2
skwhit <whitlowspencer@gmail.com>
2022-10-04 17:31:06 -0700

    made allowance for decimals, fractions, and large numbers formatted with commas

--- js/main.js
+++ js/main.js
@@ -6,9 +6,8 @@ const button = document.querySelector('button');
 let num = 0;
 let counter = 0;
 let numbers = [];
-let intOne;
-let intTwo;
 let result;
+let fraction;
 label.innerHTML = "Please enter a number"
 
 input.addEventListener("keypress", function(event) {
@@ -20,14 +19,26 @@ input.addEventListener("keypress", function(event) {
 
 // Prompt the user for two different numbers and display which, if any, is larger
 button.addEventListener("click", () => {
-  num = input.value;
+  console.log(input.value);
+  if (/\//.test(input.value)) {
+      num = input.value.split('/');
+      num = Number(num[0],10)/Number(num[1],10);
+    }
+  else if (/\,/.test(input.value)) {
+    num = input.value.split(',');
+    num = Number(num.join(''));
+  }
+  else {
+    num = Number(input.value);
+  }
+  console.log(num);
   input.value=''
   input.focus();
   
   if (counter < 2) {
-    if (/^[+-]{0,1}\d+$/.test(num)) {
-      numbers.push(num);
-      counter ++;
+        if(!isNaN(num)&&num!=0) {
+        numbers.push(num);
+        counter ++;
     } 
     else {
       alert("Your input is not a number, please enter a number");
@@ -37,16 +48,21 @@ button.addEventListener("click", () => {
   if (counter===2) {
     // Convert the values to integers
     // Store the two integers as variables
-    intOne = parseInt(numbers[0]);
-    intTwo = parseInt(numbers[1]);
+    for (let i=0; i<numbers.length;i++) {
+      if (/\//.test(numbers[i])) {
+        fraction = numbers[i];
+        fraction = fraction.split('/')
+        numbers[i] = Number(fraction[0],10)/Number(fraction[1],10);
+      }
+    }
 
     // Compare the two numbers
-    if (intTwo === intOne) {
-      result = `${intTwo} is equal to ${intOne}`;
-    } else if (intTwo > intOne) {
-      result = `${intTwo} is larger than ${intOne}`;
+    if (numbers[1] === numbers[0]) {
+      result = `${numbers[1]} is equal to ${numbers[0]}`;
+    } else if (numbers[1] > numbers[0]) {
+      result = `${numbers[1]} is larger than ${numbers[0]}`;
     } else {
-      result = `${intOne} is larger than ${intTwo}`;
+      result = `${numbers[0]} is larger than ${numbers[1]}`;
     }
     console.log(result)
     h1.innerHTML = result;
